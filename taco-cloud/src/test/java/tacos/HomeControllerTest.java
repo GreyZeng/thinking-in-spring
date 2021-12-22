9293b6cc75cbad4a0490cb67cb09b4938497fcef
zenghui <410486047@qq.com>
2021-12-22 17:26:37 +0800

    taco-cloud cho1

---
+++ taco-cloud/src/test/java/tacos/HomeControllerTest.java
@@ -0,0 +1,23 @@
+package tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("home")).andExpect(content().string(containsString("Welcome to...")));
+    }
+}
