c3276e67f4bc1490f6b75966485ed5fcfb0ca48f
GreyZeng <410486047@qq.com>
2022-11-29 23:12:11 +0800

    spring boot jdbc h2

--- src/main/java/git/snippet/spring/ThinkingInSpringApplication.java
+++ src/main/java/git/snippet/spring/ThinkingInSpringApplication.java
@@ -1,20 +1,52 @@
 package git.snippet.spring;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 // 健康检查：http://localhost:8080/actuator/health
+// 查看上下文中有哪些 bean：http://localhost:8080/actuator/beans
 @SpringBootApplication
-@RestController
-public class ThinkingInSpringApplication {
+// @RestController
+@Slf4j
+public class ThinkingInSpringApplication implements CommandLineRunner {
+  private final DataSource dataSource;
+  private final JdbcTemplate jdbcTemplate;
+
+  public ThinkingInSpringApplication(DataSource dataSource, JdbcTemplate jdbcTemplate) {
+    this.dataSource = dataSource;
+    this.jdbcTemplate = jdbcTemplate;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(ThinkingInSpringApplication.class, args);
   }
 
-  @RequestMapping("/hello")
-  public String hello() {
-    return "Hello Spring";
+  //  @RequestMapping("/hello")
+  //  public String hello() {
+  //    return "Hello Spring";
+  //  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    showConnection();
+    showData();
+  }
+
+  private void showData() {
+    jdbcTemplate.queryForList("SELECT * FROM USER_INFO").forEach(row -> log.info(row.toString()));
+  }
+
+  private void showConnection() throws SQLException {
+    log.info(dataSource.toString());
+    Connection conn = dataSource.getConnection();
+    log.info(conn.toString());
+    conn.close();
   }
 }
