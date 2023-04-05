a634be3018a8cfb3396d91b6d4abbe67c32a2035
GreyZeng <410486047@qq.com>
2023-04-05 20:17:25 +0800

    bean的继承关系

--- hello-context/src/main/java/git/snippet/helloworld/Hello.java
+++ hello-context/src/main/java/git/snippet/helloworld/Hello.java
@@ -1,7 +1,13 @@
 package git.snippet.helloworld;
 
 public class Hello {
+    private String name;
+
     public String hello() {
-        return "hello Spring";
+        return "hello world by " + name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
