0fc84c99e4c4d1bc61e8b4e196b7d8a10aa71e0e
GreyZeng <410486047@qq.com>
2023-10-28 18:20:05 +0800

    update code

---
+++ spring-web-demo-no-parents/src/test/java/git/thinking/in/spring/inherit/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package git.thinking.in.spring.inherit.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
